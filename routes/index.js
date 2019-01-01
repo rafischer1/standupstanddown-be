7f4388e4c09b947d1a8507f3ace7c168770c0d32
Robert Fischer <artiefischer@gmail.com>
2019-01-01 11:08:35 -0700

    h

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (res) {
-  res.render('index', { title: 'StandUp' });
-});
+router.get('/', (res) => res.render('index', { title: 'StandUp' }));
 
 module.exports = router;
-
-/// not gonna need this eventually
